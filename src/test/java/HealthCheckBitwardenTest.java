86332254583962ed87ea092a646e4aeaecc91e9a
Елизавета <korzheva.yelizaveta@mail.ru>
2022-05-17 13:03:31 +0300

    .

--- src/test/java/HealthCheckBitwardenTest.java
+++ src/test/java/HealthCheckBitwardenTest.java
@@ -6,7 +6,14 @@ import static io.restassured.RestAssured.given;
 public class HealthCheckBitwardenTest {
     @Test
     public static void healthCheckBitwarden() {
-
+        given()
+                .baseUri("http://192.168.0.45:80")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/alive")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
 }
